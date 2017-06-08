0d2a6ec67be6554ef95fa5471916343ae5f1e0c9
Benny <carlsson.benny93@gmail.com>
2017-06-08 20:40:28 +0200

    started on firebase

--- yn/src/main/java/hello/GreetingController.java
+++ yn/src/main/java/hello/GreetingController.java
@@ -1,10 +1,21 @@
 package hello;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseCredentials;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @Controller
 public class GreetingController {
@@ -16,7 +27,26 @@ public class GreetingController {
     }
 
     @PostMapping("/greeting")
-    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+    public String greetingSubmit(@ModelAttribute Greeting greeting) throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("C:/Users/ABrownApple/Downloads/yesno-2dbf9-firebase-adminsdk-2n3pj-c66cce13c7.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredential(FirebaseCredentials.fromCertificate(serviceAccount))
+                .setDatabaseUrl("https://yesno-2dbf9.firebaseio.com/")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        DatabaseReference ref = FirebaseDatabase
+                .getInstance()
+                .getReference("restricted_access/secret_document");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Object document = dataSnapshot.getValue();
+                System.out.println(document);
+            }
+        });
         return "result";
     }
 
