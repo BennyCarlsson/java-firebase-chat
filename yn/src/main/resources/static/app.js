0b25e18aef75400ddb410f4615b87fcc98fb94e1
Benny <carlsson.benny93@gmail.com>
2017-07-13 21:47:20 +0200

    started on firebase auth

--- yn/src/main/resources/static/app.js
+++ yn/src/main/resources/static/app.js
@@ -33,4 +33,34 @@ $(function () {
     });
     $( "#saveDB" ).click(function() { sendDB(); });
 });
-window.onload = connect();
+function firebaseGmailLogin(){
+// Initialize Firebase
+  // TODO: Replace with your project's customized code snippet
+  var config = {
+    apiKey: "AIzaSyCZKQQT1l3tF7IW3JycMsYIYpSc2-b_u98",
+    authDomain: "yesno-2dbf9.firebaseapp.com",
+    databaseURL: "https://yesno-2dbf9.firebaseio.com",
+    storageBucket: "yesno-2dbf9.appspot.com",
+  };
+  firebase.initializeApp(config);
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      connect();
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+}
+window.onload = firebaseGmailLogin();
